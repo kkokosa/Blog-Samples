e39f022fd44f0ed4b1327708ea244fb117345f50
Konrad Kokosa <kkokosa@f28-studio.com>
2017-03-10 11:29:18 +0100

    Initial tail recurrsion example.

--- VS2017/CSharp7/Program.cs
+++ VS2017/CSharp7/Program.cs
@@ -1,57 +1,33 @@
 ﻿using System;
+using System.Numerics;
 
-namespace VS2017
+namespace CSharp7
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TailRecurssionExamples examples = new TailRecurssionExamples();
 
-            Console.WriteLine("Hello World!");
+            var result = examples.CalculateByApproach0(6);
+            Console.WriteLine(result);                        
         }
     }
 
-    public static class TailRecursion
+    class TailRecurssionExamples
     {
-        public static T Execute<T>(Func<RecursionResult<T>> func)
+        public BigInteger CalculateByApproach0(int n)
         {
-            do
-            {
-                var recursionResult = func();
-                if (recursionResult.IsFinalResult)
-                    return recursionResult.Result;
-                func = recursionResult.NextStep;
-            } while (true);
+            return TailRecursion.Execute(() => Factorial(n, 1));
         }
 
-        public static RecursionResult<T> Return<T>(T result)
+        RecursionResult<BigInteger> Factorial(int n, BigInteger product)
         {
-            return new RecursionResult<T>(true, result, null);
+            if (n < 2)
+                return TailRecursion.Return(product);
+            return TailRecursion.Next(() => Factorial(n - 1, n * product));
         }
 
-        public static RecursionResult<T> Next<T>(Func<RecursionResult<T>> nextStep)
-        {
-            return new RecursionResult<T>(false, default(T), nextStep);
-        }
 
     }
-
-    public class RecursionResult<T>
-    {
-        private readonly bool _isFinalResult;
-        private readonly T _result;
-        private readonly Func<RecursionResult<T>> _nextStep;
-        internal RecursionResult(bool isFinalResult, T result, Func<RecursionResult<T>> nextStep)
-        {
-            _isFinalResult = isFinalResult;
-            _result = result;
-            _nextStep = nextStep;
-        }
-
-        public bool IsFinalResult { get { return _isFinalResult; } }
-        public T Result { get { return _result; } }
-        public Func<RecursionResult<T>> NextStep { get { return _nextStep; } }
-    }
-
-
 }
