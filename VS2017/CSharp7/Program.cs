fbbd4b3e2e09e865f69914c8387f8fa2dc12da71
Konrad Kokosa <kkokosa@f28-studio.com>
2017-03-11 17:30:35 +0100

    First approach added.

--- VS2017/CSharp7/Program.cs
+++ VS2017/CSharp7/Program.cs
@@ -10,7 +10,10 @@ namespace CSharp7
             TailRecurssionExamples examples = new TailRecurssionExamples();
 
             var result = examples.CalculateByApproach0(6);
-            Console.WriteLine(result);                        
+            Console.WriteLine(result);
+
+            result = examples.CalculateByApproach1(6);
+            Console.WriteLine(result);
         }
     }
 
@@ -21,6 +24,12 @@ namespace CSharp7
             return TailRecursion.Execute(() => Factorial(n, 1));
         }
 
+        public BigInteger CalculateByApproach1(int n)
+        {
+            RecursionResult_Approach1<BigInteger> state = new RecursionResult_Approach1<BigInteger>();
+            return TailRecursion_Approach1.Execute(() => FactorialApproach1(n, 1, state));
+        }
+
         RecursionResult<BigInteger> Factorial(int n, BigInteger product)
         {
             if (n < 2)
@@ -28,6 +37,11 @@ namespace CSharp7
             return TailRecursion.Next(() => Factorial(n - 1, n * product));
         }
 
-
+        RecursionResult_Approach1<BigInteger> FactorialApproach1(int n, BigInteger product, RecursionResult_Approach1<BigInteger> state)
+        {
+            if (n < 2)
+                return TailRecursion_Approach1.Return(product, state);
+            return TailRecursion_Approach1.Next(() => FactorialApproach1(n - 1, n * product, state), state);
+        }
     }
 }
---
+++ VS2017/CSharp7/TailRecursion_Approach1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharp7
+{
+    /// <summary>
+    /// Taken from https://www.thomaslevesque.com/2011/09/02/tail-recursion-in-c/
+    /// </summary>
+    public static class TailRecursion_Approach1
+    {
+        public static T Execute<T>(Func<RecursionResult_Approach1<T>> func)
+        {
+            do
+            {
+                var bounce = func();
+                if (bounce.NextStep == null)
+                    return bounce.Result;
+                func = bounce.NextStep;
+            } while (true);
+        }
+
+        public static RecursionResult_Approach1<T> Return<T>(T result, RecursionResult_Approach1<T> state)
+        {
+            state.Result = result;
+            state.NextStep = null;
+            return state;
+        }
+
+        public static RecursionResult_Approach1<T> Next<T>(Func<RecursionResult_Approach1<T>> nextStep, RecursionResult_Approach1<T> state)
+        {
+            state.Result = default(T);
+            state.NextStep = nextStep;
+            return state;
+        }
+    }
+
+    public class RecursionResult_Approach1<T>
+    {
+        public T Result;
+        public Func<RecursionResult_Approach1<T>> NextStep;
+    }
+
+}
